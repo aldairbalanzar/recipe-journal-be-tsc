96702c432a7240cd55fb628d58efd79f812a0ce0
aldairbalanzar <abalanzar7@gmail.com>
2021-05-03 23:47:58 -0400

    added ingredientsRouter

---
+++ src/routes/ingredientsRouter.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+// const secrets = require('../secrets')
+// const Users = require('../model/usersModel');
+// const handleUserData = require('../middleware/handleUserData');
+// const hashPassword = require('../middleware/hashPassword');
+
+// check router connection 
+router.get('/', (req: Request, res: Response) => {
+    res.status(200).json({ message: `this is where ingredients will be fetched from.`})
+});
+
+module.exports = router;
